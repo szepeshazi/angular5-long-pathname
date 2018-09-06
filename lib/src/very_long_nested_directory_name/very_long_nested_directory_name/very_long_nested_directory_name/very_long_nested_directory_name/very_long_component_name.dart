607f202d46ae58be22320afe0a4b639296a4228a
Szepesházi András <aszepeshazi@skawa.hu>
2018-09-06 11:03:59 +0200

    (fix) inline template instead of templateUrl

--- lib/src/very_long_nested_directory_name/very_long_nested_directory_name/very_long_nested_directory_name/very_long_nested_directory_name/very_long_component_name.dart
+++ lib/src/very_long_nested_directory_name/very_long_nested_directory_name/very_long_nested_directory_name/very_long_nested_directory_name/very_long_component_name.dart
@@ -2,7 +2,7 @@ import 'package:angular/angular.dart';
 
 @Component(
     selector: 'example',
-    templateUrl: '<h1>hello</h1>',
+    template: '<h1>hello</h1>',
     changeDetection: ChangeDetectionStrategy.OnPush,
     preserveWhitespace: false)
 class ExampleComponent { }
